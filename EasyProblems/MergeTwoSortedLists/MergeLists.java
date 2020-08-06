18d63074db56a2dd4c4e71a49c79af77573948fa
John West <contactjohnwest@gmail.com>
2020-08-06 14:21:07 -0700

    added MergeTwoSortedLists solution

---
+++ EasyProblems/MergeTwoSortedLists/MergeLists.java
@@ -0,0 +1,50 @@
+package EasyProblems.MergeTwoSortedLists;
+
+class MergeLists {
+    ListNode head;
+
+    public void addToTheLast(ListNode node) {
+        if (head == null) {
+            head = node;
+        }
+        else {
+            ListNode temp = head;
+            while (temp.next != null)
+                temp = temp.next;
+            temp.next = node;
+        }
+    }
+
+    /* Method to print linked list */
+    void printList() {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+
+    // Driver Code
+    public static void main(String args[]) {
+
+        MergeLists llist1 = new MergeLists();
+        MergeLists llist2 = new MergeLists();
+
+        // Node head1 = new Node(5);
+        llist1.addToTheLast(new ListNode(5));
+        llist1.addToTheLast(new ListNode(10));
+        llist1.addToTheLast(new ListNode(15));
+
+        // Node head2 = new Node(2);
+        llist2.addToTheLast(new ListNode(2));
+        llist2.addToTheLast(new ListNode(3));
+        llist2.addToTheLast(new ListNode(20));
+
+        llist1.head = new Solution().mergeTwoLists(llist1.head,
+                llist2.head);
+        llist1.printList();
+
+    }
+}
